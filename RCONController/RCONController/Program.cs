ab38af17b0e2d8ebe1091fcc499fa9991b95ef48
ikewai <yuto.ike.17@gmail.com>
2021-07-21 01:15:53 +0900

    RCONController追加

---
+++ RCONController/RCONController/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RCONController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
