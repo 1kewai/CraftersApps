5ae8824ef8afa27a680a72ac7ec41752308ae5f6
ikewai <yuto.ike.17@gmail.com>
2021-08-18 22:56:16 +0900

    リファクタリング

--- RCONController/RCONController/darknet.cs
+++ RCONController/RCONController/darknet.cs
@@ -3,11 +3,7 @@ using Discord.WebSocket;
 using Log;
 using Resource;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UI
 {
